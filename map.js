af8837acb83aea31bc303ae0bd4a6a11e5f54f20
Kevin Dick <kevin.dick9@gmail.com>
2020-07-19 20:48:22 -0400

    ibid

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 // This is the published Google Sheet from which the data are obtained.
 //var publishedData = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSx-MgeekVffmrTB7oA7AhlGP7aEpcZIDnFBLCBQL5mEUKNnYVzoR-hT_kMuA6sIEdlLmyihIJ1oO49/pub?gid=1652908691&single=true&output=csv';
-var publishedData = 'https://spreadsheets.google.com/tq?key=1gPzJmXBSq8SPVccbsu4g5tYZQZi0ac0u2c2hOFRPx2I&tqx=out:csv';
+//var publishedData = 'https://spreadsheets.google.com/tq?key=1gPzJmXBSq8SPVccbsu4g5tYZQZi0ac0u2c2hOFRPx2I&tqx=out:csv';
+var publishedData = 'https://spreadsheets.google.com/feeds/cells/1gPzJmXBSq8SPVccbsu4g5tYZQZi0ac0u2c2hOFRPx2I/orc3knd/public/full?alt=json';
 
 var rad_Earth  = 6378.16;
 var one_degree = (2 * Math.PI * rad_Earth) / 360;
